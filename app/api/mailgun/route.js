4e0717b311dfa7309ddd8b6e9f7564f0b8b12ec2
данил <whokilleduverdosze4@gmail.com>
2026-03-27 00:21:40 +0500

    fix

--- app/api/mailgun/route.js
+++ app/api/mailgun/route.js
@@ -2,23 +2,29 @@ import { NextResponse } from "next/server";
 
 // глобальное хранилище
 global.messages = global.messages || [];
-
-
+export const dynamic = "force-dynamic";
 export async function POST(req) {
-    const formData = await req.formData();
+    try {
+        const formData = await req.formData();
 
-    const message = {
-        id: Date.now(),
-        from: formData.get("from"),
-        to: formData.get("recipient"), // 🔥 ВОТ ЭТО ОБЯЗАТЕЛЬНО
-        subject: formData.get("subject"),
-        text: formData.get("body-plain"),
-        html: formData.get("body-html"),
-    };
+        const message = {
+            id: Date.now(),
+            from: formData.get("from"),
+            to: formData.get("recipient"),
+            subject: formData.get("subject"),
+            text: formData.get("body-plain"),
+            html: formData.get("body-html"),
+        };
 
-    global.messages.unshift(message);
+        global.messages.unshift(message);
 
-    return NextResponse.json({ success: true });
+        console.log("NEW MAIL:", message); // 👈 важно
+
+        return NextResponse.json({ ok: true });
+    } catch (e) {
+        console.log("ERROR:", e);
+        return NextResponse.json({ error: true }, { status: 500 });
+    }
 }
 
 export async function GET() {
