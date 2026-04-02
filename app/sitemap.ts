6a83885a8f604b1ed474e3ff9e9a0a77008c5a5b
данил <whokilleduverdosze4@gmail.com>
2026-04-03 02:48:27 +0500

    fixurl

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://tempfastmail.site",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://tempfastmail.site/privacy",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://tempfastmail.site/terms",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://tempfastmail.site/about",
+            lastModified: new Date(),
+        },
+    ];
+}
